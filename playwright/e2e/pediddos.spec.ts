58a09bcc98a23f8cde09f607cc8a06a0d7d8246b
Juliano Saadeh <julilano.s.dev@gmail.com>
2026-03-25 10:41:16 -0300

    Atualizando Assert de pedidos.spec.ts

--- playwright/e2e/pediddos.spec.ts
+++ playwright/e2e/pediddos.spec.ts
@@ -1,33 +1,27 @@
 import { test, expect } from '@playwright/test';
 
+//AAA - Arrange, Act, Assert
+
 test('deve consltar um pedido aprovado', async ({ page }) => {
+
+  //Arrange - Prepara o teste
   await page.goto('http://localhost:5173/');
-
-//Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-  //Clica no link de consultar pedido
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  // preenche localzador de pedido
-  await page.getByTestId('search-order-id').fill('VLO-AE2RCC')
 
-  //Clica no botão de buscar pedido
+  //Act - Executa a ação
+  await page.getByTestId('search-order-id').fill('VLO-AE2RCC')
   await page.getByTestId('search-order-button').click()
 
 
-  //Checkpoint - valida se esta visivel na tela o resultado do pedido
+  //Assert 
   await expect(page.getByTestId('order-result-id')).toBeVisible()
-
-  //Checkpoint - valida se o pedido foi encontrado
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-AE2RCC')
 
-  //Checkpoint - valida se o status do pedido é aprovado
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-
 
 
 });
